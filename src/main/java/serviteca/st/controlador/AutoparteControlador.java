867bc981f52e19245e25b15defa8a01dc7915957
Yusun16 <yusunguairabryan@gmail.com>
2024-08-16 15:01:43 -0500

    Autopartes

---
+++ src/main/java/serviteca/st/controlador/AutoparteControlador.java
@@ -0,0 +1,11 @@
+package serviteca.st.controlador;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("serviteca")
+
+
+public class AutoparteControlador {
+}
