1d1841386ba8bfb4d3ea86e9ed465cb5c28af854
Yusun16 <yusunguairabryan@gmail.com>
2024-08-20 12:27:16 -0500

    Orden de servicio(Inicio)

--- src/main/java/serviteca/st/controlador/OrdenControlador.java
+++ src/main/java/serviteca/st/controlador/OrdenControlador.java
@@ -1,11 +1,36 @@
 package serviteca.st.controlador;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import serviteca.st.modelo.Orden;
+import serviteca.st.servicio.IOrdenServicio;
+import serviteca.st.servicio.OrdenServicio;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("serviteca")
+@CrossOrigin(value = "http://localhost:3000")
 
 
 public class OrdenControlador {
+    private static final Logger logger = LoggerFactory.getLogger((OrdenControlador.class));
+
+    @Autowired
+    private IOrdenServicio ordenServicio;
+
+    @GetMapping("/ordenservicios")
+    public List<Orden> obtenerOrdenes(){
+        var ordenes = ordenServicio.listarOrden();
+        ordenes.forEach((orden -> logger.info(orden.toString())));
+        return ordenes;
+    }
+
+    @PostMapping("/ordenservicios")
+    public Orden agregarOrden(@RequestBody Orden orden){
+        logger.info("Orden a agregar: " + orden);
+        return ordenServicio.guardarOrden(orden);
+    }
 }
