563837eda103accec9fdeed9b2079d5da9144103
Javhuila <jeferson_valbuenahu@fet.edu.co>
2024-11-12 12:36:07 -0500

    sign in without rol.

--- src/main/java/serviteca/st/controlador/AuthControlador.java
+++ src/main/java/serviteca/st/controlador/AuthControlador.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -62,6 +63,63 @@ public class AuthControlador {
     public ResponseEntity<?> loginnn() {
         return new ResponseEntity<>("Hello, Welcome to Serviteca", HttpStatus.OK);
     };
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registeruser(@Valid @RequestBody SignupRequest signupRequest) {
+        if (userRepositorio.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Este Usuario ya existe!"));
+        }
+
+        User user = new User(signupRequest.getUsername(),
+                passwordEncoder.encode(signupRequest.getPassword()));
+
+        user.setCedula(signupRequest.getCedula());
+
+        Set<String> strRoles = signupRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepositorio.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: El role no existe o no se encuentra registrado!"));
+            roles.add(userRole);
+        } else  {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepositorio.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: El role no se encuentra!"));
+                        roles.add(adminRole);
+                        break;
+                    case "jefeTaller":
+                        Role jefeTallerRole = roleRepositorio.findByName(ERole.ROLE_TALLER)
+                                .orElseThrow(() -> new RuntimeException("Error: El role no se encuentra!"));
+                        roles.add(jefeTallerRole);
+                        break;
+                    case "jefeInventario":
+                        Role jefeInventarioRole = roleRepositorio.findByName(ERole.ROLE_INVENTARIO)
+                                .orElseThrow(() -> new RuntimeException("Error: El role no se encuentra!"));
+                        roles.add(jefeInventarioRole);
+                        break;
+                    case "asistenteCompra":
+                        Role asistenteCompraRole = roleRepositorio.findByName(ERole.ROLE_ASISTENTE)
+                                .orElseThrow(() -> new RuntimeException("Error: El role no se encuentra!"));
+                        roles.add(asistenteCompraRole);
+                        break;
+                    default:
+                        Role userRole = roleRepositorio.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: El role no se encuentra!"));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepositorio.save(user);
+
+        return ResponseEntity.ok().body(new MessageResponse("Registrado exitosamente!"));
+    }
 
     @PostMapping("/signin")
     public ResponseEntity<?> autenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
